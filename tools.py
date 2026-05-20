72dc529b046f464ac3a57d6763a8e71f34af535d
iiishop <30495867+iiishop@users.noreply.github.com>
2026-05-20 23:37:28 +0100

    Fix #1: remove unused tools.py, add requirements.txt

--- tools.py
+++
@@ -1,8 +0,0 @@
-def linear_interpolation(a, b, total_time, space):
-    num = int(total_time / space)
-    increment = (b - a) / num
-
-    for i in range(num):
-        yield a + i * increment
-
-    yield b
